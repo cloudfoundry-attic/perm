c59be810c1a175c8e6597f8f7b728100b32f928d
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-09-05 17:58:08 -0700

    Add initial gRPC server
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

--- cmd/perm/main.go
+++ cmd/perm/main.go
@@ -1,13 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
+	"net"
+
+	"context"
+
+	"reflect"
+
+	"code.cloudfoundry.org/perm/protos"
+	"google.golang.org/grpc"
 )
 
-func main() {
-	for {
-		fmt.Println("bosh job")
-		time.Sleep(time.Second * 10)
+type roleServiceServer struct {
+	roleBindings map[string]map[string][]map[string]string
+}
+
+func (s *roleServiceServer) AssignRole(ctx context.Context, req *protos.AssignRoleRequest) (*protos.AssignRoleResponse, error) {
+	actor := req.GetActor()
+	role := req.GetRole()
+
+	roleBinding, ok := s.roleBindings[actor]
+	if !ok {
+		roleBinding = make(map[string][]map[string]string)
+		s.roleBindings[actor] = roleBinding
 	}
+
+	roleBinding[role] = append(roleBinding[role], req.GetContext().GetContext())
+
+	return &protos.AssignRoleResponse{}, nil
+}
+
+func (s *roleServiceServer) HasRole(ctx context.Context, req *protos.HasRoleRequest) (*protos.HasRoleResponse, error) {
+	actor := req.GetActor()
+	role := req.GetRole()
+	roleContext := req.GetContext().GetContext()
+
+	roleBinding, ok := s.roleBindings[actor]
+	if !ok {
+		return &protos.HasRoleResponse{HasRole: false}, nil
+	}
+
+	contexts, ok := roleBinding[role]
+	if !ok {
+		return &protos.HasRoleResponse{HasRole: false}, nil
+	}
+
+	var found bool
+	for _, c := range contexts {
+		if reflect.DeepEqual(c, roleContext) {
+			found = true
+			break
+		}
+	}
+
+	return &protos.HasRoleResponse{HasRole: found}, nil
+}
+
+func newServer() *roleServiceServer {
+	s := &roleServiceServer{
+		roleBindings: make(map[string]map[string][]map[string]string),
+	}
+	return s
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	grpcServer := grpc.NewServer(opts...)
+	protos.RegisterRoleServiceServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
 }
