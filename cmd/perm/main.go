ca7e3e726296072a4527ffb5d1119eb8e1a7e1e4
Chris Hendrix <chendrix@pivotal.io>
2017-09-07 11:28:58 -0700

    Revert "Add versioning to the binary"
    This reverts commit 6a5f9ed13c5077b5a128b58ec822cedd10b0ecf8.

--- cmd/perm/main.go
+++ cmd/perm/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 
-	"code.cloudfoundry.org/perm"
 	"code.cloudfoundry.org/perm/protos"
 	"code.cloudfoundry.org/perm/rpc"
 	"github.com/jessevdk/go-flags"
@@ -16,7 +15,6 @@ import (
 type options struct {
 	Hostname string `long:"listen-hostname" description:"Hostname on which to listen for gRPC traffic" default:"0.0.0.0"`
 	Port     int    `long:"listen-port" description:"Port on which to listen for gRPC traffic" default:"6283"`
-	Version  bool   `short:"V" long:"version" description:"Prints the current Perm version"`
 }
 
 func main() {
@@ -28,13 +26,6 @@ func main() {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
-	}
-
-	if parserOpts.Version {
-		fmt.Println(perm.Version)
-
-		os.Exit(0)
-		return
 	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", parserOpts.Hostname, parserOpts.Port))
--- version.go
+++
@@ -1,3 +0,0 @@
-package perm
-
-var Version = "dev"
