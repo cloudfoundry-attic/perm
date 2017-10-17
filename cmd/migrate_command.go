feccd9473abd7049220abd28a99e651a6a8d0a40
Chris Hendrix <chendrix@pivotal.io>
2017-10-17 13:44:25 -0700

    Add DB connection to perm migrate
    [#150979010]

--- cmd/migrate_command.go
+++ cmd/migrate_command.go
@@ -1,5 +1,7 @@
 package cmd
 
+import "code.cloudfoundry.org/perm/messages"
+
 type MigrateCommand struct {
 	Logger LagerFlag
 
@@ -9,6 +11,23 @@ type MigrateCommand struct {
 func (cmd MigrateCommand) Execute([]string) error {
 	logger, _ := cmd.Logger.Logger("perm")
 	logger = logger.Session("migrate")
+
+	db, err := cmd.SQL.Open()
+	if err != nil {
+		logger.Error(messages.ErrFailedToOpenSQLConnection, err)
+		return err
+	}
+
+	pingLogger := logger.Session(messages.PingSQLConnection, cmd.SQL.LagerData())
+	pingLogger.Debug(messages.Starting)
+	err = db.Ping()
+	if err != nil {
+		logger.Error(messages.ErrFailedToPingSQLConnection, err, cmd.SQL.LagerData())
+		return err
+	}
+	pingLogger.Debug(messages.Finished)
+
+	defer db.Close()
 
 	return nil
 }
