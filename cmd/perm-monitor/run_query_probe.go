09d3c04ba9105e738e0ea7ad5f6e1e4d3045012b
Chris Hendrix <chendrix@pivotal.io>
2018-01-05 14:50:06 -0800

    Clean up query probe
    [#153708843]

--- cmd/perm-monitor/run_query_probe.go
+++ cmd/perm-monitor/run_query_probe.go
@@ -26,37 +26,34 @@ func RunQueryProbe(ctx context.Context, logger lager.Logger, wg *sync.WaitGroup,
 
 	var innerWG sync.WaitGroup
 	innerWG.Add(2)
-	go rotateHistogramPeriodically(&innerWG, QueryProbeHistogramRefreshTime, statter)
-	go runProbePeriodically(ctx, logger, &innerWG, probe, statter)
+	go func() {
+		defer innerWG.Done()
+
+		for range time.NewTicker(QueryProbeHistogramRefreshTime).C {
+			statter.Rotate()
+		}
+	}()
+
+	go func() {
+		defer innerWG.Done()
+
+		for range time.NewTicker(QueryProbeTickDuration).C {
+			correct, durations, err := runQueryProbe(ctx, logger, probe)
+
+			if err != nil {
+				statter.SendFailedQueryProbe(logger.Session("metrics"))
+			} else if !correct {
+				statter.SendIncorrectQueryProbe(logger.Session("metrics"))
+			} else {
+				for _, d := range durations {
+					statter.RecordQueryProbeDuration(logger.Session("metrics"), d)
+				}
+				statter.SendCorrectQueryProbe(logger.Session("metrics"))
+			}
+		}
+	}()
 
 	innerWG.Wait()
-}
-
-func runProbePeriodically(ctx context.Context, logger lager.Logger, wg *sync.WaitGroup, probe *monitor.QueryProbe, statter *monitor.Statter) {
-	defer wg.Done()
-
-	for range time.NewTicker(QueryProbeTickDuration).C {
-		correct, durations, err := runQueryProbe(ctx, logger, probe)
-
-		if err != nil {
-			statter.SendFailedQueryProbe(logger.Session("metrics"))
-		} else if !correct {
-			statter.SendIncorrectQueryProbe(logger.Session("metrics"))
-		} else {
-			for _, d := range durations {
-				statter.RecordQueryProbeDuration(logger.Session("metrics"), d)
-			}
-			statter.SendCorrectQueryProbe(logger.Session("metrics"))
-		}
-	}
-}
-
-func rotateHistogramPeriodically(wg *sync.WaitGroup, d time.Duration, statter *monitor.Statter) {
-	defer wg.Done()
-
-	for range time.NewTicker(d).C {
-		statter.Rotate()
-	}
 }
 
 func runQueryProbe(ctx context.Context, logger lager.Logger, probe *monitor.QueryProbe) (bool, []time.Duration, error) {
