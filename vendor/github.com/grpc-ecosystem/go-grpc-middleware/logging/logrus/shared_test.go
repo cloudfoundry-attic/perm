df9683ce50dcd41fc7f56158544d549303748b72
Isobel Redelmeier <iredelmeier@pivotal.io>
2018-04-04 16:14:18 -0700

    Update dependencies
    [#156519466]

--- vendor/github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/shared_test.go
+++ vendor/github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/shared_test.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
-
 	"testing"
 
-	"github.com/sirupsen/logrus"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tags/logrus"
 	"github.com/grpc-ecosystem/go-grpc-middleware/testing"
 	pb_testproto "github.com/grpc-ecosystem/go-grpc-middleware/testing/testproto"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 )
@@ -35,7 +35,8 @@ func customCodeToLevel(c codes.Code) logrus.Level {
 
 func (s *loggingPingService) Ping(ctx context.Context, ping *pb_testproto.PingRequest) (*pb_testproto.PingResponse, error) {
 	grpc_ctxtags.Extract(ctx).Set("custom_tags.string", "something").Set("custom_tags.int", 1337)
-	grpc_logrus.Extract(ctx).Info("some ping")
+	ctx_logrus.AddFields(ctx, logrus.Fields{"custom_field": "custom_value"})
+	ctx_logrus.Extract(ctx).Info("some ping")
 	return s.TestServiceServer.Ping(ctx, ping)
 }
 
@@ -45,7 +46,8 @@ func (s *loggingPingService) PingError(ctx context.Context, ping *pb_testproto.P
 
 func (s *loggingPingService) PingList(ping *pb_testproto.PingRequest, stream pb_testproto.TestService_PingListServer) error {
 	grpc_ctxtags.Extract(stream.Context()).Set("custom_tags.string", "something").Set("custom_tags.int", 1337)
-	grpc_logrus.Extract(stream.Context()).Info("some pinglist")
+	ctx_logrus.AddFields(stream.Context(), logrus.Fields{"custom_field": "custom_value"})
+	ctx_logrus.Extract(stream.Context()).Info("some pinglist")
 	return s.TestServiceServer.PingList(ping, stream)
 }
 
@@ -82,11 +84,12 @@ func (s *logrusBaseSuite) SetupTest() {
 	s.mutexBuffer.Unlock()
 }
 
-func (s *logrusBaseSuite) getOutputJSONs() []string {
-	ret := []string{}
+func (s *logrusBaseSuite) getOutputJSONs() []map[string]interface{} {
+	ret := make([]map[string]interface{}, 0)
 	dec := json.NewDecoder(s.mutexBuffer)
+
 	for {
-		var val map[string]json.RawMessage
+		var val map[string]interface{}
 		err := dec.Decode(&val)
 		if err == io.EOF {
 			break
@@ -94,8 +97,9 @@ func (s *logrusBaseSuite) getOutputJSONs() []string {
 		if err != nil {
 			s.T().Fatalf("failed decoding output from Logrus JSON: %v", err)
 		}
-		out, _ := json.MarshalIndent(val, "", "  ")
-		ret = append(ret, string(out))
+
+		ret = append(ret, val)
 	}
+
 	return ret
 }
