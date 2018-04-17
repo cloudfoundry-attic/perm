b071c94fdbf5539b7ccb9fa55a21919308235563
Jeff Pak <jeffrey.pak@emc.com>
2018-04-17 14:56:20 -0700

    restore actor and role assignment tables
    [#156445722]
    
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- pkg/api/db/migrations/7_combine_actor_and_role_assignment_tables.go
+++ pkg/api/db/migrations/7_combine_actor_and_role_assignment_tables.go
@@ -116,23 +116,86 @@ func CombineActorAndRoleAssignmentTablesDown(ctx context.Context, logger lager.L
 	defer logger.Debug(finished)
 	var err error
 
-	if tx.Flavor() == sqlx.DBFlavorMariaDB && strings.HasPrefix(tx.Version(), "10.1") {
-		_, err = tx.ExecContext(ctx, createActorsTableMariaDB)
-	} else {
-		_, err = tx.ExecContext(ctx, createActorsTable)
-	}
+	err = CreateActorsTableUp(ctx, logger, tx)
 	if err != nil {
 		return err
 	}
 
-	_, err = tx.ExecContext(ctx, createRoleAssignmentsTable)
+	err = CreateRoleAssignmentsTableUp(ctx, logger, tx)
 	if err != nil {
 		return err
 	}
 
-	_, err = tx.ExecContext(ctx, addRoleIDForeignKey)
+	var entityData [][]string
+	rows, err := squirrel.Select("actor_id", "actor_namespace").
+		From("assignment").
+		RunWith(tx).
+		QueryContext(ctx)
 	if err != nil {
 		return err
+	}
+
+	for rows.Next() {
+		var (
+			actorID        string
+			actorNamespace string
+		)
+
+		err = rows.Scan(&actorID, &actorNamespace)
+		entityData = append(entityData, []string{actorID, actorNamespace})
+		if err != nil {
+			return err
+		}
+
+	}
+	rows.Close()
+
+	for _, actor := range entityData {
+		u := uuid.NewV4().Bytes()
+		_, err = squirrel.Insert("actor").
+			Columns("uuid", "domain_id", "issuer").
+			Values(u, actor[0], actor[1]).
+			RunWith(tx).
+			ExecContext(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	var roleAssignmentData [][]int64
+	rows, err = squirrel.Select("actor.id", "assignment.role_id").
+		From("actor").
+		JoinClause("INNER JOIN assignment ON actor.domain_id = assignment.actor_id and actor.issuer = assignment.actor_namespace").
+		RunWith(tx).
+		QueryContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var (
+			actorID int64
+			roleID  int64
+		)
+
+		err = rows.Scan(&actorID, &roleID)
+		roleAssignmentData = append(roleAssignmentData, []int64{actorID, roleID})
+		if err != nil {
+			return err
+		}
+
+	}
+	rows.Close()
+
+	for _, roleAssignment := range roleAssignmentData {
+		_, err = squirrel.Insert("role_assignment").
+			Columns("actor_id", "role_id").
+			Values(roleAssignment[0], roleAssignment[1]).
+			RunWith(tx).
+			ExecContext(ctx)
+		if err != nil {
+			return err
+		}
 	}
 
 	_, err = tx.ExecContext(ctx, dropAssignmentTable)
