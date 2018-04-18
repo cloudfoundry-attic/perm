879e6c799e3ce8cc470b12a4eba54ec480d1dbc8
Slawek Ligus <sligus@pivotal.io>
2018-04-18 12:08:48 -0700

    add permission service and role service server fakes
    [#156768438]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- protos/gen/package.go
+++ protos/gen/package.go
@@ -2,4 +2,8 @@ package protos // import "code.cloudfoundry.org/perm/protos/gen"
 
 //go:generate counterfeiter . PermissionServiceClient
 
+//go:generate counterfeiter . PermissionServiceServer
+
 //go:generate counterfeiter . RoleServiceClient
+
+//go:generate counterfeiter . RoleServiceServer
