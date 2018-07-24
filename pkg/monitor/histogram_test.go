e2cd0c67d478866fd10e678fec8e983dddce5165
Nick Wei <nwei@pivotal.io>
2018-07-24 14:05:45 -0700

    test for ValueAtQuantile and Reset for HistogramSet
    [#159146012]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- pkg/monitor/histogram_test.go
+++ pkg/monitor/histogram_test.go
@@ -30,9 +30,10 @@ var _ = Describe("ThreadSafeHistogram", func() {
 
 		It("returns the highest recorded value", func() {
 			Expect(subject.Max("some-label")).To(Equal(int64(12345)))
+			Expect(subject.Max("some-other-label")).To(Equal(int64(67890)))
 		})
 
-		It("returns the highest recorded value for total", func() {
+		It("returns the highest recorded overall value", func() {
 			Expect(subject.Max("overall")).To(Equal(int64(67890)))
 		})
 	})
@@ -48,6 +49,7 @@ var _ = Describe("ThreadSafeHistogram", func() {
 			Expect(subject.ValueAtQuantile("some-label", 84)).To(Equal(int64(3)))
 			Expect(subject.ValueAtQuantile("some-label", 50)).To(Equal(int64(2)))
 		})
+
 		It("understands p100 as a max", func() {
 			for j := int64(1); j <= 5; j++ {
 				for i := int64(0); i <= 100; i++ {
@@ -68,6 +70,22 @@ var _ = Describe("ThreadSafeHistogram", func() {
 			maxValue := int64(105)
 			Expect(subject.ValueAtQuantile("some-label", 100)).To(Equal(maxValue))
 			Expect(subject.Max("some-label")).To(Equal(maxValue))
+		})
+
+		It("records the overall quantiles if separate values are recorded", func() {
+			Expect(subject.ValueAtQuantile("some-label", 50)).To(Equal(int64(0)))
+
+			subject.RecordValue("some-label", 1)
+			subject.RecordValue("some-label", 2)
+			subject.RecordValue("some-label", 3)
+			subject.RecordValue("some-other-label", 4)
+			subject.RecordValue("yet-another-label", 5)
+
+			Expect(subject.ValueAtQuantile("some-label", 99)).To(Equal(int64(3)))
+			Expect(subject.ValueAtQuantile("overall", 99)).To(Equal(int64(5)))
+
+			Expect(subject.ValueAtQuantile("some-label", 50)).To(Equal(int64(2)))
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(3)))
 		})
 	})
 
@@ -91,6 +109,29 @@ var _ = Describe("ThreadSafeHistogram", func() {
 			Expect(subject.ValueAtQuantile("some-label", 50)).To(Equal(int64(2)))
 			subject.Rotate()
 			Expect(subject.ValueAtQuantile("some-label", 50)).To(Equal(int64(0)))
+		})
+
+		It("rotates all values including the overall histogram's once they're out of the window size", func() {
+			Expect(subject.ValueAtQuantile("some-label", 50)).To(Equal(int64(0)))
+
+			subject.RecordValue("some-label", 1)
+			subject.RecordValue("some-label", 2)
+			subject.RecordValue("some-label", 3)
+			subject.RecordValue("some-other-label", 4)
+			subject.RecordValue("yet-another-label", 5)
+
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(3)))
+
+			subject.Rotate()
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(3)))
+			subject.Rotate()
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(3)))
+			subject.Rotate()
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(3)))
+			subject.Rotate()
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(3)))
+			subject.Rotate()
+			Expect(subject.ValueAtQuantile("overall", 50)).To(Equal(int64(0)))
 		})
 	})
 })
