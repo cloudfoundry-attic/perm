17313616254e8401d35e6d651388c23c698ae120
Isobel Redelmeier <iredelmeier@pivotal.io>
2018-08-30 16:27:57 -0700

    Delete unused fakes
    [#160186271]

--- pkg/api/protos/package.go
+++ pkg/api/protos/package.go
@@ -1,9 +1,1 @@
 package protos // import "code.cloudfoundry.org/perm/pkg/api/protos"
-
-//go:generate counterfeiter . PermissionServiceClient
-
-//go:generate counterfeiter . PermissionServiceServer
-
-//go:generate counterfeiter . RoleServiceClient
-
-//go:generate counterfeiter . RoleServiceServer
